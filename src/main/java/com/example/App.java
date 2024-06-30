08c5df9624af7f481c957a6dc61bf5a25f33de02
金俊辉 <1304767535@qq.com>
2024-06-30 08:31:46 +0800

    test2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -32,40 +32,43 @@ public class App {
             cpabe.keygen(pubfile, "src/main/java/com/example/keys/key1.txt", mskfile, user1Attributes);
             cpabe.keygen(pubfile, "src/main/java/com/example/keys/key2.txt", mskfile, user2Attributes);
 
-            byte[] encKey0 = cpabe.enc(pubfile, "level:0 level:1 1of2 org0 2of2", "AESKey0", "encrypted/encrypted0.txt");
+            System.out.println("user_keys created successfully.");
 
             String policy0 = "level:0 level:1 1of2 org:org0 2of2";
             String policy1 = "dept:dept1 1of1";
             String policy2 = "org:org1 1of1";
 
+            String en0 = "src/main/java/com/example/encrypted/encrypted0.txt";
+            String en1 = "src/main/java/com/example/encrypted/encrypted1.txt";
+            String en2 = "src/main/java/com/example/encrypted/encrypted2.txt";
 
             String fileContent0 = new String(Files.readAllBytes(Paths.get("src/main/java/com/example/files/file0.txt")));
-            String AESKey0 = "mySecretAESKey0";
-            byte[] encryptedFile0 = cpabe.enc(pubfile, policy0, fileContent0, "src/main/java/com/example/encrypted/encrypted0.txt");
-            Files.write(Paths.get("src/main/java/com/example/encrypted/encrypted0.txt"), Base64.getEncoder().encode(encryptedFile0));
+            byte[] encryptedFile0 = cpabe.enc(pubfile, policy0, fileContent0, en0);
+            Files.write(Paths.get(en0), Base64.getEncoder().encode(encryptedFile0));
 
             String fileContent1 = new String(Files.readAllBytes(Paths.get("src/main/java/com/example/files/file1.txt")));
-            String AESKey1 = "mySecretAESKey1";
-            byte[] encryptedFile1 = cpabe.enc(pubfile, policy1, fileContent1, "src/main/java/com/example/encrypted/encrypted1.txt");
-            Files.write(Paths.get("src/main/java/com/example/encrypted/encrypted1.txt"), Base64.getEncoder().encode(encryptedFile1));
+            byte[] encryptedFile1 = cpabe.enc(pubfile, policy1, fileContent1, en1);
+            Files.write(Paths.get(en1), Base64.getEncoder().encode(encryptedFile1));
 
             String fileContent2 = new String(Files.readAllBytes(Paths.get("src/main/java/com/example/files/file2.txt")));
-            String AESKey2 = "mySecretAESKey2";
-            byte[] encryptedFile2 = cpabe.enc(pubfile, policy2, fileContent2, "src/main/java/com/example/encrypted/encrypted2.txt");
-            Files.write(Paths.get("src/main/java/com/example/encrypted/encrypted2.txt"), Base64.getEncoder().encode(encryptedFile2));
+            byte[] encryptedFile2 = cpabe.enc(pubfile, policy2, fileContent2, en2);
+            Files.write(Paths.get(en2), Base64.getEncoder().encode(encryptedFile2));
 
+            System.out.println("encryptedtxets created successfully.");
 
-            byte[] encFileContent0 = Base64.getDecoder().decode(Files.readAllBytes(Paths.get("src/main/java/com/example/encrypted/encrypted0.txt")));
+            byte[] encFileContent0 = Base64.getDecoder().decode(Files.readAllBytes(Paths.get(en0)));
             byte[] decryptedFile0 = cpabe.dec(pubfile, "src/main/java/com/example/keys/key0.txt", encFileContent0);
             Files.write(Paths.get("src/main/java/com/example/decrypted/decrypted0.txt"), decryptedFile0);
 
-            byte[] encFileContent1 = Base64.getDecoder().decode(Files.readAllBytes(Paths.get("src/main/java/com/example/encrypted/encrypted1.txt")));
+            byte[] encFileContent1 = Base64.getDecoder().decode(Files.readAllBytes(Paths.get(en1)));
             byte[] decryptedFile1 = cpabe.dec(pubfile, "src/main/java/com/example/keys/key1.txt", encFileContent1);
             Files.write(Paths.get("src/main/java/com/example/decrypted/decrypted1.txt"), decryptedFile1);
 
-            byte[] encFileContent2 = Base64.getDecoder().decode(Files.readAllBytes(Paths.get("src/main/java/com/example/encrypted/encrypted2.txt")));
+            byte[] encFileContent2 = Base64.getDecoder().decode(Files.readAllBytes(Paths.get(en2)));
             byte[] decryptedFile2 = cpabe.dec(pubfile, "src/main/java/com/example/keys/key2.txt", encFileContent2);
             Files.write(Paths.get("src/main/java/com/example/decrypted/decrypted2.txt"), decryptedFile2);
+
+            System.out.println("decryptedtxets created successfully.");
 
             System.out.println("Decrypted content of file0.txt:");
             System.out.println(new String(decryptedFile0));
